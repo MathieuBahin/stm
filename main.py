ff0ed772e1c466360a3963357522b275617c6d2a
PICcie <ni.co.las@hotmail.fr>
2020-02-19 16:22:24 +0100

    Add dth functions

--- main.py
+++ main.py
@@ -5,11 +5,14 @@
 
 import streams
 import task as ts
+import DTH as dth
 
 # create a serial port with default parameters
 streams.serial()
 
-
+def display(number, delay):
+    print(DHT_temp,DHT_hum)
+    sleep(delay)
 
 # create the various threads using the same function but passing different parameters        
 ts.initTask()
@@ -18,4 +21,6 @@ thread(ts.task2,2,600)
 thread(ts.task3,3,1300)
 thread(ts.task4,4,800)
 
+thread(dth.DTH_task, 5, 2500) #time must be more than 2000
+thread(display, 6, 1000)
    
