462eb54550dfa60ba2a11011310188b534a617f0
PICcie <ni.co.las@hotmail.fr>
2020-02-19 15:12:00 +0100

    update task 1-4

---
+++ task.py
@@ -0,0 +1,16 @@
+
+def task1():
+    if(1):
+        print("mot1")
+
+def task2():
+    if(1):
+        print("mot2")
+
+def task3():
+    if(1):
+        print("mot3")
+
+def task4():
+    if(1):
+        print("mot4")
