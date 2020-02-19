8906f9758509a2d297458c0d772daae6d431ba64
PICcie <ni.co.las@hotmail.fr>
2020-02-19 15:37:49 +0100

    thread lock

--- task.py
+++ task.py
@@ -1,21 +1,33 @@
 import streams
+import threading
+
+mutex1 = threading.Lock()
+mutex2 = threading.Lock()
+mutex3 = threading.Lock()
+mutex4 = threading.Lock()
+
+def initTask():
+    mutex1.release()
+    mutex2.acquire()
+    mutex3.acquire()
+    mutex4.acquire()
 
 def task1(number, delay):
-    if(1):
-        print("mot1")
-    sleep(delay)
+    mutex1.acquire()
+    print("mot1")
+    mutex2.release()
 
 def task2(number, delay):
-    if(1):
-        print("mot2")
-    sleep(delay)
+    mutex2.acquire()
+    print("mot2")
+    mutex3.release()
 
 def task3(number, delay):
-    if(1):
-        print("mot3")
-    sleep(delay)
+    mutex3.acquire()
+    print("mot3")
+    mutex4.release()
 
 def task4(number, delay):
-    if(1):
-        print("mot4")
-    sleep(delay)
+    mutex4.acquire()
+    print("mot4")
+    mutex1.release()
