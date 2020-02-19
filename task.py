244205e5fb9b7d8426dce8361035428f8c1c2d50
PICcie <ni.co.las@hotmail.fr>
2020-02-19 15:13:52 +0100

    update function with parameters

--- task.py
+++ task.py
@@ -1,16 +1,21 @@
+import streams
 
-def task1():
+def task1(number, delay):
     if(1):
         print("mot1")
+    sleep(delay)
 
-def task2():
+def task2(number, delay):
     if(1):
         print("mot2")
+    sleep(delay)
 
-def task3():
+def task3(number, delay):
     if(1):
         print("mot3")
+    sleep(delay)
 
-def task4():
+def task4(number, delay):
     if(1):
         print("mot4")
+    sleep(delay)
